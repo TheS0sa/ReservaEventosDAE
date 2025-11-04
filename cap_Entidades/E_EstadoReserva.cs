6ff11ae523a3717d3e2bbfcf056be304b0ad3e57
TheS0sa <sosahotmeil@hotmail.com>
2025-11-04 10:27:13 -0600

    desde aqui empezamos de nuevo, entidades arregladas, ya ejecuta, hacer formularios

--- cap_Entidades/E_EstadoReserva.cs
+++ cap_Entidades/E_EstadoReserva.cs
@@ -2,10 +2,6 @@
 
 public class E_EstadoReserva
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> df0131c551573872440a5328b9370c67604e8eb0
     public int IdEstado { get; set; }
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
